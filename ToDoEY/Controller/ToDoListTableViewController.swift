ac36576b1f00f8f4c3bf2d2ee5e34a8ca690c884
Alex Asichka <sanechka3@mail.ru>
2023-04-01 17:52:57 +0300

    add gradient for items

--- ToDoEY/Controller/ToDoListTableViewController.swift
+++ ToDoEY/Controller/ToDoListTableViewController.swift
@@ -7,6 +7,7 @@
 
 import UIKit
 import RealmSwift
+import ChameleonFramework
 
 class ToDoListTableViewController: SwipeTableViewController {
     
@@ -17,36 +18,38 @@ class ToDoListTableViewController: SwipeTableViewController {
     }
     let realm = try! Realm()
     var items: Results<Item>?
- 
-   // MARK: - viewDidLoad
+    
+    // MARK: - viewDidLoad
     
     override func viewDidLoad() {
         super.viewDidLoad()
         navigationController?.navigationBar.backgroundColor =  #colorLiteral(red: 0.2103916407, green: 0.5888115764, blue: 1, alpha: 1)
     }
-
+    
     // MARK: - Table view data source
-
+    
     override func numberOfSections(in tableView: UITableView) -> Int {
         return 1
     }
-
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return items?.count ?? 1
     }
-
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = super.tableView(tableView, cellForRowAt: indexPath)
-//        let cell = tableView.dequeueReusableCell(withIdentifier: "toDoItemCell", for: indexPath)
         if let item = items?[indexPath.row] {
             cell.textLabel?.text = item.title
+            let color = UIColor(hexString: selectedCategory?.color).darken(byPercentage: CGFloat(indexPath.row)/CGFloat((items?.count ?? 1)))
+            cell.backgroundColor = color
+            cell.textLabel?.textColor = ContrastColorOf(backgroundColor: color ?? .black, returnFlat: true)
             cell.accessoryType = item.isDone ? .checkmark : .none
         } else {
             cell.textLabel?.text = "No added elements"
         }
         return cell
     }
-
+    
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         guard let item = items?[indexPath.row] else {
             return
@@ -60,8 +63,8 @@ class ToDoListTableViewController: SwipeTableViewController {
         tableView.reloadData()
         tableView.deselectRow(at: indexPath, animated: true)
     }
- 
-//     MARK: - Add new items
+    
+    //     MARK: - Add new items
     
     @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
         var textField = UITextField()
@@ -122,7 +125,7 @@ extension ToDoListTableViewController: UISearchBarDelegate {
         items = items?.filter("title CONTAINS[cd] %@", text).sorted(byKeyPath: "dateCreated", ascending: true)
         tableView.reloadData()
     }
-
+    
     func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
         if searchBar.text?.count == 0 {
             loadItems()
